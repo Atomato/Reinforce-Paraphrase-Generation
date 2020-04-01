113a62cab1cdddd286d4cdac1a52fdbbf50284bb
keunhyung_chung <keunhyung_chung@tmax_co.kr>
2020-04-01 12:51:28 +0000

    없어진 save_model_iter 복구, log_root 복구

--- src/config.py
+++ src/config.py
@@ -1,11 +1,10 @@
 import os
 
-# root_dir = os.path.expanduser("~")
-# root_dir = os.path.join(root_dir, "Desktop")
+# root_dir is "src/.."
 root_dir = os.path.join(os.getcwd(),'..')
 
 print_interval = 100
-
+save_model_iter = 100
 
 train_data_path = os.path.join(root_dir, "data/kor/chunked/train_*")
 eval_data_path = os.path.join(root_dir, "data/kor/chunked/val_*")
@@ -13,7 +12,7 @@ decode_data_path = os.path.join(root_dir, "data/kor/chunked/test_*")
 vocab_path = os.path.join(root_dir, "data/kor/vocab")
 emb_v_path = os.path.join(root_dir, "data/kor/word_emb.txt")
 emb_list_path = os.path.join(root_dir, "data/kor/word_list.txt")
-log_root = os.path.join(root_dir, "log_kor")
+log_root = os.path.join(root_dir, "log/MLE")
 
 
 # Hyperparameters
