61f751cec9b3a94e81ec281a4260eab5828b5508
keunhyung_chung <keunhyung_chung@tmax_co.kr>
2020-04-03 16:58:39 +0900

    Fix the number of 'tap' in result

--- src/utils.py
+++ src/utils.py
@@ -109,13 +109,13 @@ def write_for_result(input_sents, reference_sents, decoded_words, _result_path, 
 
   if os.path.isfile(_result_path):
     with open(_result_path, "a") as f:
-      print("x:\t\t" + input_s, file=f)
-      if data_class == 'val': print("y:\t\t" + reference_s, file=f)
+      print("x:\t" + input_s, file=f)
+      if data_class == 'val': print("y:\t" + reference_s, file=f)
       print("y_pred:\t" + decoded_s + "\n", file=f)
   else:
     with open(_result_path, "w") as f:
-      print("x:\t\t" + input_s, file=f)
-      if data_class == 'val': print("y:\t\t" + reference_s, file=f)
+      print("x:\t" + input_s, file=f)
+      if data_class == 'val': print("y:\t" + reference_s, file=f)
       print("y_pred:\t" + decoded_s + "\n", file=f)
 
 def gen_ngram(sent, n=2):
