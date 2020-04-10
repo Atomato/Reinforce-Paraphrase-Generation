02e3961d2170d854c364f0d89e1f61df91915c39
DaehanKim <kdh5852@gmail.com>
2020-04-10 11:27:54 +0900

    postprocess update : replace colon / compute new bleu score after postprocess / add more rules

--- src/post_process.py
+++ src/post_process.py
@@ -1,8 +1,10 @@
 import os
 import random
 import datetime
+import pyjosa
 from kobert.utils import get_tokenizer
 from gluonnlp.data import SentencepieceTokenizer
+import nltk
 
 # custom modules
 import config
@@ -44,6 +46,7 @@ class PostProcess():
 	def process_by_idx(self, idx, txt):
 		if idx % 4 == 2: # y_pred lines
 			txt = self.correct_numbering(txt)
+			txt = self.replace_colon(txt)
 			txt = self.rep_wrong_char(txt)
 			txt = self.tag_strange_txt(txt)
 		return txt
@@ -66,17 +69,32 @@ class PostProcess():
 			if k in txt: txt = txt.replace(k,random.choice(v))
 		return txt
 
+	def replace_colon(self, txt):
+		txt = txt.replace('y_pred:\t','')
+		spl = list(map(lambda x : x.strip(), txt.split(':')))
+		if len(spl) != 2 : return txt # if it has more than 2 colons, just return it as-is
+		if spl[0] in spl[1]: txt = '' # if second part contains the key word, remove the key word
+		else: txt = '{}(은)는 '.format(spl[0])
+		txt += '{}'.format(spl[1])
+		txt = pyjosa.replace_josa(txt)
+		return txt
+
 	def post_process(self):
 		inst_list = []
+		bleu_list = []
 		for idx,line in enumerate(self.file):
 			line = line.strip()
 			line = self.process_by_idx(idx,line)
 			self.map_dict_by_idx(idx,line)
 			if idx % 4 == 3: # if reading an instance ends
+				sent_bleu = nltk.translate.bleu_score.sentence_bleu([self.tokenizer(self.inst_dict['y'])],
+														self.tokenizer(self.inst_dict['y_pred']), 
+														weights=(0.5,0.5))
+				bleu_list.append(sent_bleu)
 				inst_list.append(self.inst_dict)
 				self.inst_dict = {} # flush instance dict
-
-		inst_list.append(self.inst_dict)
+		# record the bleu score of post-processed predictions
+		inst_list.append('Average BLEU score: {:f}'.format(sum(bleu_list)/len(bleu_list)))
 		return inst_list
 
 	def run(self):
@@ -86,7 +104,7 @@ class PostProcess():
 		for _dict in inst_list[:-1]:
 			for k,v in _dict.items(): txt += '{}:\t{}\n'.format(k,v)
 			txt += '\n'
-		txt += inst_list[-1]['x']
+		txt += inst_list[-1]
 
 		with open(self.output_path,'wt',encoding='utf8') as f:
 			f.write(txt)
@@ -96,7 +114,7 @@ class PostProcess():
 
 def main():
 	root_dir = os.getcwd()
-	input_path = os.path.join(root_dir,'pretrain_result.txt')
+	input_path = os.path.join(root_dir,'new_hyperparam_result.txt')
 	now = datetime.datetime.now()
 	proc = PostProcess(input_path=input_path, output_path = 'processed_{:02d}{:02d}{:02d}{:02d}.txt'.format(now.month,now.day,now.hour,now.minute))
 	proc.run()
